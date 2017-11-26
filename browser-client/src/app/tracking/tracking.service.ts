281cf87ae417a6e4d7046f7f67af1931d1188708
RazvanSebastian <rzvs95@gmail.com>
2017-11-27 01:15:57 +0200

    Google map and positions marks #6

--- browser-client/src/app/tracking/tracking.service.ts
+++ browser-client/src/app/tracking/tracking.service.ts
@@ -4,11 +4,20 @@ import { Http, RequestOptions, Headers, URLSearchParams } from '@angular/http';
 import { HostURI } from '../shared/common';
 import { StorageManager } from '../shared/storage-manager';
 
+
 import { Observable } from 'rxjs/Observable'
 
 export class Device{
 	public token:string;
+	public name:string;
 	public registrationDate:Date;
+
+	constructor(){}
+}
+
+export class Position{
+
+	constructor(public latitude:number,public longitude){}
 }
 
 @Injectable()
@@ -31,5 +40,22 @@ export class TrackingService {
 		let options = new RequestOptions({headers:headers});
 
 		return this._http.get(this.hostURI.adress+"/device/all",options);
+	}
+
+	findDevicePositions(token,startdate,enddate){
+		console.log(token);
+		if(this.storageManager.empty())
+			return null;
+		let headers = new Headers();
+		headers.append("token",token);
+		headers.append("Authorization","Bearer "+localStorage.getItem("access_token"));
+
+		let params = new URLSearchParams();
+		params.set('startdate' , startdate);
+		params.set('enddate' , enddate);
+
+		let options = new RequestOptions({headers:headers,params:params});
+		
+		return this._http.get(this.hostURI.adress+"/position",options);
 	}
 }
