d629b5906b7e493db21d448f2057ccfeaca7cb12
RazvanSebastian <rzvs95@gmail.com>
2017-11-27 16:29:04 +0200

    RouterGuard uncommited

--- browser-client/src/app/security/router-guard.service.ts
+++ browser-client/src/app/security/router-guard.service.ts
@@ -2,16 +2,21 @@ import { Injectable} from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from '@angular/router';
 import { AccountService } from '../account/account.service';
 
+
 @Injectable()
 export class RouterGuardService implements CanActivate{
 
 	constructor(
-		private _accountService : AccountService
+		private _accountService : AccountService,
+		private _router : Router
 		) { }
 
 	canActivate(){
-		if(localStorage.getItem('access_token') == null)
+		if(localStorage.getItem('access_token') == null){
+			window.alert("You must to be authenticate to access this page!")
+			this._router.navigateByUrl('/login');
 			return false;
+		}
 		return this._accountService.checkAuthorization()
 		.map(res => {
 				return res ? true : false
--- browser-client/src/app/shared/error-handler.service.ts
+++ browser-client/src/app/shared/error-handler.service.ts
@@ -7,7 +7,9 @@ export class ErrorHandlerService implements ErrorHandler {
 	constructor() { }
 
 	handleError(error : Error) {
+		console.log(error.message)
 		let statusCode = +error.message.substring(error.message.indexOf("status:")).split(" ")[1];
+		console.log(statusCode);
 		if(statusCode == 401)
 			window.alert("You must login again or your session has expired!");
 	}
